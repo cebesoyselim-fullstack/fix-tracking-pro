3834dd214bf1d28c1549f3d94609e0ee0123da27
Selim Cebesoy <cebesoyselim1@gmail.com>
2025-12-31 13:31:21 +0300

    chore: enable cors, global validation pipe and api prefix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -10,6 +10,9 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  // Enable CORS for frontend communication
+  app.enableCors();
+
   // Enable global validation pipe
   app.useGlobalPipes(
     new ValidationPipe({
